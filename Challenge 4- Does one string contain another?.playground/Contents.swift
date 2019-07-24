b9b3f3331761911ecb7b8b15827e0954d94f26e6
Maged <maged.mo.it@gmal.com>
2019-07-24 21:07:52 +0200

    challenge 4

--- Challenge 4- Does one string contain another?.playground/Contents.swift
+++ Challenge 4- Does one string contain another?.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+//Write your own version of the contains() method on String that ignores letter case, and
+//without using the existing contains() method.
+
+
+extension String{
+    
+    func fuzzyContains(input:String) -> Bool{
+       let arrayString = self.split(separator: " ")
+        for word in arrayString{
+            if word.lowercased() == input.lowercased(){
+                return true
+            }
+        }
+        return false
+    }
+}
+let str = "hello world"
+str.fuzzyContains(input: "WoRl")
