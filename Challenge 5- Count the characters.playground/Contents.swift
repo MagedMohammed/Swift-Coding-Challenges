436ce3aeaa2339666253b6f2a5e01c2b74e32f73
Maged <maged.mo.it@gmal.com>
2019-08-05 12:31:33 +0200

    Challenge 5

--- Challenge 5- Count the characters.playground/Contents.swift
+++ Challenge 5- Count the characters.playground/Contents.swift
@@ -4,10 +4,9 @@ import UIKit
 //and returns how many times a specific character appears,
 //taking case into account.
 
-func counterChare(input:String, count:Character) -> Int{
- 
-    
-    
-    
-    return 0
+func counterChare(input:String, charToCount:Character) -> Int{
+    let chars = input.lowercased().filter{$0 == charToCount }
+    return chars.count
 }
+let result = counterChare(input: "Mississippi", charToCount: "i")
+print(result)
